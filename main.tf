a4a9e1428ef1e36e73d042aa9f0d1edb2b771a57
Alexandar Podobnik <alexandar.podobnik@gmail.com>
2018-07-04 14:50:12 -0400

    Fix illegal variable definition

--- main.tf
+++ main.tf
@@ -28,7 +28,7 @@ resource "google_compute_instance" "gce" {
   can_ip_forward = "${var.can_ip_forward}"
   create_timeout = "${var.create_timeout}"
   description = "${var.description}"
-  deletion_protection = ${var.deletion_protection}
+  deletion_protection = "${var.deletion_protection}"
 
   metadata = "${var.metadata}"
   metadata_startup_script = "${var.metadata_startup_script}"
